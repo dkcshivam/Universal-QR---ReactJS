e0c9463ec9462fd3387ac5ad1c72e4fbd56f9a26
shishpal-2000 <shishpal@dkcexport.co.in>
2025-07-25 11:52:38 +0530

    newapi-changes

--- src/components/Core/Product/ProductDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ProductDetail = () => {
-  const prodCode = window.location.href.split("prod_code=")[1];
-
-  return <div>{`ProductDetail-${prodCode}`}</div>;
-};
-
-export default ProductDetail;
